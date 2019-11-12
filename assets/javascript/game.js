943c1675259c4091a1ead1fcfdfece20f68e3493
Michael Gelber <mpgelber7495@gmail.com>
2019-11-11 19:49:11 -0500

    Staged the enemies to attack area

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 var yourCharacter;
 
-function createCharacterBox(character) {
+function createCharacterBox(character, characterClass = "") {
   var charachterHTML =
     '<div id="' +
     character["name"] +
-    '"class="mx-1 character-holder d-flex flex-column align-items-center"> <p class="mb-0 character-name">' +
+    '"class="mx-1 character-holder ' +
+    characterClass +
+    ' d-flex flex-column align-items-center"> <p class="mb-0 character-name">' +
     character["name"] +
     '</p> <img class="character-image w-100" src="' +
     character["imageURL"] +
@@ -21,11 +23,49 @@ function addCharactersToSelectionArea() {
     );
   }
   $(".your-character-row").toggle();
+  $(".enemies-to-attack").toggle();
 }
 addCharactersToSelectionArea();
 
+function addCharactersToAreaClass(character, areaClass, characterClass) {
+  $(areaClass)[0].innerHTML += createCharacterBox(character, characterClass);
+}
+
+function selectCharacterByName(selectedCharacterName) {
+  for (var i = 0; i < characters.length; i++) {
+    if (characters[i]["name"] === selectedCharacterName) {
+      return characters[i];
+    }
+  }
+}
+
+function addCharactersToEnemiesToAttackArea(selectedCharacterName) {
+  for (var i = 0; i < characters.length; i++) {
+    if (characters[i]["name"] !== selectedCharacterName) {
+      addCharactersToAreaClass(
+        characters[i],
+        ".enemies-to-attack",
+        "enemies-character-holder"
+      );
+    }
+  }
+}
+
 $(".character-holder").click(function(event) {
-  yourCharacter = event.currentTarget.id;
+  yourCharacterName = event.currentTarget.id;
+  yourCharacter = selectCharacterByName(yourCharacterName);
   $(".select-character-row").toggle();
   $(".your-character-row").toggle();
+  $(".enemies-to-attack").toggle();
+  addCharactersToAreaClass(yourCharacter, ".your-character-row");
+  addCharactersToEnemiesToAttackArea(yourCharacterName);
+});
+
+$(".character-holder").click(function(event) {
+  console.log(event);
+  console.log(event.currentTarget.id);
+  var defenseCharacterName = event.currentTarget.id;
+  defenseCharacter = selectCharacterByName(defenseCharacterName);
+  console.log(defenseCharacterName);
+  console.log("#" + defenseCharacterName);
 });

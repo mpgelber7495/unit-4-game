72104051a15206cdacfc864ee500afced0c0864b
Michael Gelber <mpgelber7495@gmail.com>
2019-11-13 22:02:53 -0500

    Fixed non-clearing battle field on loss and ability to continue clicking after loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var yourCharacterOriginalAttackPower;
 var enemyCharacter;
 var isBattleGroundLaunched = false;
 var countOfDefeatedEnemies = 0;
+var gameIsOver = false;
 function createCharacterBox(character, characterClass = "") {
   var charachterHTML =
     '<div id="' +
@@ -81,7 +82,7 @@ yourCharacterEventListener();
 
 // Listen for you to select an enemy if isEnemyEngaged is false
 $(".enemies-to-attack").on("click", ".enemy", function(event) {
-  if (!isEnemyEngaged) {
+  if (isEnemyEngaged === false && gameIsOver === false) {
     var enemyCharacterName = $(this).attr("id");
     enemyCharacter = selectCharacterByName(enemyCharacterName);
     $(this).remove();
@@ -99,29 +100,34 @@ $(".enemies-to-attack").on("click", ".enemy", function(event) {
 });
 
 function enemiesFight(yourCharacter, engagedEnemy) {
-  var enemyNewHealth =
-    engagedEnemy["healthPoints"] - yourCharacter["attackPower"];
-  var yourCharacterNewHealth =
-    yourCharacter["healthPoints"] - engagedEnemy["attackPower"];
-  var yourCharacterNewAttackPower =
-    yourCharacter["attackPower"] + yourCharacterOriginalAttackPower;
-  engagedEnemy["healthPoints"] = enemyNewHealth;
-  yourCharacter["healthPoints"] = yourCharacterNewHealth;
-  yourCharacter["attackPower"] = yourCharacterNewAttackPower;
-  refreshCharacterHealthPointsOnDOM(yourCharacter);
-  refreshCharacterHealthPointsOnDOM(engagedEnemy);
-  if (enemyNewHealth <= 0) {
-    $("#" + engagedEnemy["name"]).remove();
-    countOfDefeatedEnemies++;
-    isEnemyEngaged = false;
+  if (gameIsOver === false) {
+    var enemyNewHealth =
+      engagedEnemy["healthPoints"] - yourCharacter["attackPower"];
+    var yourCharacterNewHealth =
+      yourCharacter["healthPoints"] - engagedEnemy["attackPower"];
+    var yourCharacterNewAttackPower =
+      yourCharacter["attackPower"] + yourCharacterOriginalAttackPower;
+    engagedEnemy["healthPoints"] = enemyNewHealth;
+    yourCharacter["healthPoints"] = yourCharacterNewHealth;
+    yourCharacter["attackPower"] = yourCharacterNewAttackPower;
+    refreshCharacterHealthPointsOnDOM(yourCharacter);
+    refreshCharacterHealthPointsOnDOM(engagedEnemy);
+    if (enemyNewHealth <= 0) {
+      $("#" + engagedEnemy["name"]).remove();
+      countOfDefeatedEnemies++;
+      isEnemyEngaged = false;
 
-    if (countOfDefeatedEnemies === charactersInstant.length - 1) {
-      $("#restart-button").toggle();
+      if (countOfDefeatedEnemies === charactersInstant.length - 1) {
+        isEnemyEngaged = false;
+        gameIsOver = true;
+        $("#restart-button").toggle();
+      }
     }
-  }
-  if (yourCharacterNewHealth <= 0) {
-    $("#restart-button").toggle();
-    $("#" + yourCharacter["name"] + "-health-points").text(0);
+    if (yourCharacterNewHealth <= 0) {
+      $("#restart-button").toggle();
+      $("#" + yourCharacter["name"] + "-health-points").text(0);
+      gameIsOver = true;
+    }
   }
 }
 
@@ -135,15 +141,15 @@ function restartGame() {
   charactersInstant = newCharactersArr();
   $("#restart-button").toggle();
   $(".select-character-row").toggle();
-  // $(".your-character-row").toggle();
-  // $(".enemies-to-attack").toggle();
   $(".your-character-holder").html("");
+  $(".battle-ground-container").html("");
   yourCharacter = undefined;
   isEnemyEngaged = false;
   yourCharacterOriginalAttackPower;
   enemyCharacter;
   isBattleGroundLaunched = false;
   countOfDefeatedEnemies = 0;
+  gameIsOver = false;
   addCharactersToSelectionArea();
   yourCharacterEventListener();
 }
